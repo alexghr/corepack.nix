e746634440d877805657a679c767c6f0d6fb2013
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-03-28 20:28:17 +0100

    feat: add individual package managers

--- flake.nix
+++ flake.nix
@@ -10,6 +10,19 @@
         pkgs = import nixpkgs { inherit system; };
       in rec {
         packages.corepack = import ./corepack.nix { inherit pkgs; };
+        packages.yarn = packages.corepack.overrideAttrs (finalAttrs: prevAttrs: {
+          name = "yarn";
+          meta.mainProgram = "yarn";
+        });
+        packages.pnpm = packages.corepack.overrideAttrs (finalAttrs: prevAttrs: {
+          name = "pnpm";
+          meta.mainProgram = "pnpm";
+        });
+        packages.pnpx = packages.corepack.overrideAttrs (finalAttrs: prevAttrs: {
+          name = "pnpx";
+          meta.mainProgram = "pnpx";
+        });
+
         packages.default = packages.corepack;
       }
     );
